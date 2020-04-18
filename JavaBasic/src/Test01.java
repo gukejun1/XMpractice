336c2c1c448c40709dcd37b52c4b62d794851ef3
秭归晓 <1475977956@qq.com>
2020-04-18 14:22:47 +0800

    gukejun  javaBasic

---
+++ JavaBasic/src/Test01.java
@@ -0,0 +1,8 @@
+/**
+ * Created by fang on 2020/4/18.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("xiaoming");
+    }
+}
---
+++ src/Test01.java
@@ -0,0 +1,8 @@
+/**
+ * Created by fang on 2020/4/17.
+ */
+public class Test01 {
+    public static void main(String [] args){
+        System.out.print("xiaoming");
+    }
+}
